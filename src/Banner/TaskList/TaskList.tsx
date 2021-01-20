e62c1486cdcb9d283518501b755f7a89ca8536d0
Charl <info@charlroux.co.za>
2021-01-20 19:00:54 +0200

    Adding styling Tasklist

--- src/Banner/TaskList/TaskList.tsx
+++ src/Banner/TaskList/TaskList.tsx
@@ -1,8 +1,14 @@
-import React from "react";
-import { Form } from "react-bootstrap";
+import React, { useState } from "react";
+import { Button, ButtonGroup, Form, ToggleButton } from "react-bootstrap";
 import FormEntry from "../../common/FormEntry/FormEntry";
 import "./style.css";
 const TaskList = (props: any) => {
+   const [radioValue, setRadioValue] = useState("1");
+   const radios = [
+     { name: "All", value: "1" },
+     { name: "Active", value: "2" },
+     { name: "Completed", value: "3" },
+   ];
     return (
       <div className="tasklistContainer">
         <Form className="enteredTasks">
@@ -10,10 +16,32 @@ const TaskList = (props: any) => {
           <FormEntry mode={props.mode} />
           <FormEntry mode={props.mode} />
         </Form>
-        <div className="taskListFooter">
-          <div className="firstContainer">test</div>
-          <div className="secondContainer">test</div>
-          <div className="thirdContainer">test</div>
+        <div className={"taskListFooter " + props.mode}>
+          <div className="firstContainer">5 items left</div>
+          <div className="secondContainer">
+            <div className="buttonContainer">
+              <ButtonGroup toggle>
+                {radios.map((radio, idx) => (
+                  <ToggleButton
+                    key={idx}
+                    type="radio"
+                    variant="secondary"
+                    name="radio"
+                    value={radio.value}
+                    checked={radioValue === radio.value}
+                    onChange={(e) => setRadioValue(e.currentTarget.value)}
+                  >
+                    {radio.name}
+                  </ToggleButton>
+                ))}
+              </ButtonGroup>
+            </div>
+          </div>
+          <div className="thirdContainer">
+            <Button variant="secondary" size="lg" active>
+              Clear Completed
+            </Button>
+          </div>
         </div>
       </div>
     );
